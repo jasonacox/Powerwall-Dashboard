6ddad969c8d389d4b554053777f117e11af66a09
Jason A. Cox <jason@jasonacox.com>
2023-02-15 21:24:41 -0800

    Move alerts data into INF retention policy

---
+++ influxdb/run-once-2.8.3.sql
@@ -0,0 +1,12 @@
+# DDL
+# Powerwall-Dashboard v2.8.3
+# 
+# Version 2.8.3 move alert data into the "alerts" retention policy. This script moves the
+# existing data (in the "raw" retention policy) into the new "alerts" policy.
+#
+# Manual:
+# docker exec --tty influxdb sh -c "influx -import -path=/var/lib/influxdb/run-once-2.8.3.sql"
+#
+# USE powerwall
+CREATE DATABASE powerwall
+SELECT * INTO "powerwall"."alerts"."alerts" FROM "powerwall"."raw"."alerts" GROUP BY *
