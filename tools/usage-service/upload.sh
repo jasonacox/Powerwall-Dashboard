f279e0b4db3c2943ef6936c562d6c4e260b545e6
Jason A. Cox <jason@jasonacox.com>
2023-06-11 00:50:55 -0700

    Remove armv7 architecture

--- tools/usage-service/upload.sh
+++ tools/usage-service/upload.sh
@@ -10,12 +10,12 @@ echo "Version: ${VER}"
 
 # Build jasonacox/CONTAINER:x.y.z
 echo "* BUILD jasonacox/${CONTAINER}:${VER}"
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --push -t jasonacox/${CONTAINER}:${VER} .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t jasonacox/${CONTAINER}:${VER} .
 echo ""
 
 # Build jasonacox/CONTAINER:latest
 echo "* BUILD jasonacox/${CONTAINER}:latest"
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --push -t jasonacox/${CONTAINER}:latest .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t jasonacox/${CONTAINER}:latest .
 echo ""
 
 # Verify
